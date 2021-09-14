25a28a29ec1b9e305f5c3cffa0e3bc3000d1f9fa
Erik Siefken <git@posporus.de>
2021-09-14 11:33:42 +0200

    registerd ExerciseLogModel

--- src/store/database/index.ts
+++ src/store/database/index.ts
@@ -3,11 +3,13 @@ import { Database } from '@vuex-orm/core'
 import WorkoutModel from 'src/store/models/WorkoutModel'
 import WorkoutLogModel from '../models/WorkoutLogModel'
 import ExerciseModel from '../models/ExerciseModel'
+import { ExerciseLogModel } from '../models'
 
 const database = new Database()
 
 database.register(WorkoutModel)
 database.register(ExerciseModel)
 database.register(WorkoutLogModel)
+database.register(ExerciseLogModel)
 
 export default database
