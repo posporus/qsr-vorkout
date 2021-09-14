5c3052c2a0305997c5a309b9b6b18918d11378a3
Erik Siefken <git@posporus.de>
2021-09-14 10:27:20 +0200

    tidy defaults

---
+++ src/static/defaults/exercise_defaults.ts
@@ -0,0 +1,9 @@
+import { ExerciseOptions } from 'src/types'
+
+const exercise_defaults:ExerciseOptions = {
+    preset:'timer',
+    reps:false,
+    time:30,
+}
+
+export default exercise_defaults
---
+++ src/static/defaults/index.ts
@@ -0,0 +1,7 @@
+import exercise_defaults from './exercise_defaults';
+import exercise_select_defaults from './exercise_select_defaults';
+
+export {
+    exercise_defaults,
+    exercise_select_defaults
+}
