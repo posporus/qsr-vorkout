dd31f0ef83df4380de843a60118f62a03f4245ab
Erik Siefken <git@posporus.de>
2021-09-24 11:54:57 +0200

    added some getters

--- src/store/models/WorkoutLogModel.ts
+++ src/store/models/WorkoutLogModel.ts
@@ -1,17 +1,19 @@
 import { Model } from '@vuex-orm/core'
-import {WorkoutModel} from '.'
+import { WorkoutModel } from '.'
 import { ExerciseLogModel } from '.'
 import { nanoid } from 'nanoid'
+import moment from 'moment'
+import { duration } from 'src/utility'
 //import WorkoutModel from './WorkoutModel'
 //import { SetNeat } from 'src/classes/Set'
 
 export default class WorkoutLogModel extends Model {
   //$id!: string | null
-  workout_id!: string | null
-  workout!:WorkoutModel | null
-  exercises!:ExerciseLogModel | null
+  workout_id!: string
+  workout!: WorkoutModel
+  exercises!: ExerciseLogModel[]
   started!: Date
-  ended!:Date | false
+  ended!: Date | false
   /*
   constructor() {
     super()
@@ -20,14 +22,29 @@ export default class WorkoutLogModel extends Model {
   */
   static entity = 'workout_logs'
 
-  static fields() {
+  static fields () {
     return {
-      id:this.uid(() => nanoid()),
+      id: this.uid(() => nanoid()),
       workout_id: this.string(null).nullable(),
-      workout:this.belongsTo(WorkoutModel,'workout_id') || {},
-      exercises:this.hasMany(ExerciseLogModel,'workout_log_id'),
+      workout: this.belongsTo(WorkoutModel, 'workout_id') || {},
+      exercises: this.hasMany(ExerciseLogModel, 'workout_log_id'),
       started: this.attr(Date.now()),
       ended: this.attr(false)
     };
   }
+
+  public get duration (): string | undefined {
+    return duration(this.started,this.ended)
+  }
+
+  public get fromNow (): string {
+    return moment(this.ended || this.started).fromNow()
+  }
+
+  
+  public get exerciseCount() : number {
+    return this.exercises.length
+  }
+  
+
 }
