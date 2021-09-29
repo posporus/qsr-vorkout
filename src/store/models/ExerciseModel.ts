65d2cfcf465e7acb1cfc737d91a700388823e733
Erik Siefken <git@posporus.de>
2021-09-29 12:35:14 +0200

    changes to exercise model + interface

--- src/store/models/ExerciseModel.ts
+++ src/store/models/ExerciseModel.ts
@@ -5,11 +5,13 @@
 import { Model } from '@vuex-orm/core'
 import { nanoid } from 'nanoid'
 import { exercise_defaults } from 'src/static/defaults'
+import moment from 'moment'
 
 import ExerciseOptions from 'src/types/ExerciseOptions'
 
 export default class ExerciseModel extends Model {
   id!:string
+  date!:Date
   name!: string
   countable!:boolean
   defaults!:ExerciseOptions
@@ -20,9 +22,15 @@ export default class ExerciseModel extends Model {
   static fields() {
     return {
       id:this.uid(() => nanoid()),
+      date:this.attr(Date.now()),
       name:this.string('').nullable(),
       countable:this.boolean(true),
       defaults:this.attr(exercise_defaults),
     };
   }
+  
+  public get dateString() : string {
+    return moment(this.date).calendar()
+  }
+  
 }
--- src/types/ExerciseInterface.ts
+++ src/types/ExerciseInterface.ts
@@ -2,7 +2,6 @@ import { preset } from '.';
 export default interface ExerciseInterface {
     id?:string
     preset?:preset
-    isRest?:true
     reps?:number
     time?:number
 }
