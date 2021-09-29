8ccfc83780df0a2d97696ee6aabadcad700f17f6
Erik Siefken <git@posporus.de>
2021-09-29 12:42:44 +0200

    a few new components

---
+++ src/components/InputSpinnerComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-item>
+    <q-input dense :label="label" outlined v-model="value">
+      <template v-slot:prepend>
+        <q-btn  round flat @click="value--" icon="remove_circle" />
+      </template>
+      <template v-slot:append>
+        <q-btn round flat @click="value++" icon="add_circle" />
+      </template>
+    </q-input>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'InputSpinnerComponent',
+  data() {
+    return {
+      value: this.modelValue || 0,
+    }
+  },
+  props: {
+    modelValue: Number,
+    label:String,
+  },
+  emits: ['update:modelValue'],
+
+  mounted() {
+    this.$watch('value', () => {
+      this.$emit('update:modelValue', this.value)
+    })
+    this.$watch('modelValue', (val: number) => {
+      this.value = val
+    })
+  },
+})
+</script>
+
+<style></style>
