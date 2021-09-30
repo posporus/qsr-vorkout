cfdacc1a841a740b6e0506863155d875d20c6cd2
Erik Siefken <git@posporus.de>
2021-09-30 18:37:17 +0200

    they now have a key. may remove later

--- src/types/ExerciseInterface.ts
+++ src/types/ExerciseInterface.ts
@@ -1,6 +1,7 @@
 import { preset } from '.';
 export default interface ExerciseInterface {
     id?:string
+    key?:string
     preset?:preset
     reps?:number
     time?:number
--- src/types/SetInterface.ts
+++ src/types/SetInterface.ts
@@ -1,6 +1,7 @@
 import { ExerciseInterface } from '.';
 export default interface SetInterface {
     id?:string
+    key?:string
     sets: number
     exercises: Array<ExerciseInterface>
 }
