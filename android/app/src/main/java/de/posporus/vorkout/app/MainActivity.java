03d9dfe16c3388694cce2b642f4b3cbf253a31da
Erik Siefken <git@posporus.de>
2021-10-02 17:08:34 +0200

    added capacitor and android manually and its working

---
+++ android/app/src/main/java/de/posporus/vorkout/app/MainActivity.java
@@ -0,0 +1,5 @@
+package de.posporus.vorkout.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.posporus.vorkout.app',
+  appName: 'qsr-vorkout',
+  webDir: 'dist\\spa',
+  bundledWebRuntime: false
+};
+
+export default config;
