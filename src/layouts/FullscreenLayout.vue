c9a7ba93a76a582cd8feef527c39e63ac059aad6
Erik Siefken <git@posporus.de>
2021-10-02 18:17:55 +0200

    screen keep awake and status bar hiding are working

--- src/layouts/FullscreenLayout.vue
+++ src/layouts/FullscreenLayout.vue
@@ -1,18 +1,26 @@
 <template>
-    <q-layout>
-        <q-page-container>
-            <router-view />
-        </q-page-container>
-    </q-layout>
+  <q-layout>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { StatusBar } from '@capacitor/status-bar'
+import { KeepAwake } from '@capacitor-community/keep-awake'
 export default defineComponent({
-    name:'FullscreenLayout'
+  name: 'FullscreenLayout',
+  mounted() {
+    void StatusBar.hide()
+    void KeepAwake.keepAwake()
+  },
+  unmounted() {
+    void StatusBar.show()
+    void KeepAwake.allowSleep()
+  },
 })
 </script>
 
-<style>
-
-</style>
+<style></style>
