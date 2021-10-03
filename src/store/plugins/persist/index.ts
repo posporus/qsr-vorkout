bb78d2e85ebc228bf26d953a7d9e1e0ad866ac70
Erik Siefken <git@posporus.de>
2021-10-03 11:14:50 +0200

    added capacitor storage

--- src/store/plugins/persist/index.ts
+++ src/store/plugins/persist/index.ts
@@ -1,22 +1,19 @@
 import createPersistedState from 'vuex-persistedstate'
+import { Storage } from '@capacitor/storage';
 
 const PersistedState = createPersistedState({
-  /*
+
   storage: {
-    getItem: (key) => {
-      const storage = JSON.parse(localStorage.getItem(key) || '') as unknown
-      console.log('getItem', key, storage)
-      return storage
+    getItem: async (key: string) => {
+      return await Storage.get({ key: key });
     },
-    setItem: (key, value) => {
-      localStorage.setItem(key, JSON.stringify(value))
-      console.log('setItem', key, value)
+    setItem: async (key: string, value: string) => {
+      await Storage.set({ key: key, value: value });
     },
-    removeItem: (key) => {
-      localStorage.removeItem(key)
-      console.log('remove item')
+    removeItem: async (key: string) => {
+      await Storage.remove({ key: key });
     },
-  },*/
+  },
   key: 'vorkout',
   //paths: ['entities']
 });
